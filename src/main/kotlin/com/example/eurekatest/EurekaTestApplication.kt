18260bcb381a227d7a45e3bfb2ef298f1bb752e0
lee-kangchan <lsc4237@gmail.com>
2023-05-16 18:22:39 +0900

    eureka 설정

--- src/main/kotlin/com/example/eurekatest/EurekaTestApplication.kt
+++ src/main/kotlin/com/example/eurekatest/EurekaTestApplication.kt
@@ -2,8 +2,10 @@ package com.example.eurekatest
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class EurekaTestApplication
 
 fun main(args: Array<String>) {
